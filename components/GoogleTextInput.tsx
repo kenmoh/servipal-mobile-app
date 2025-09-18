0bb59bf1813cd9f75699d41f99ed37f0e733fb45
kenmoh <kenneth.aremoh@gmail.com>
2025-09-18 09:19:30 +0100

    update .env

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -89,7 +89,6 @@ const GoogleTextInput = ({ placeholder, onPlaceSelect, value, error, onChangeTex
 
       <GooglePlacesTextInput
         apiKey={`${process.env.EXPO_PUBLIC_GOOGLE_MAP_API_KEY}`}
-        // apiKey='AIzaSyAgNolNtWaGhGU_UKC0WM-TVmOUq1rWWRQ'
         // onPlaceSelect={handlePlaceSelect}
         ref={ref}
         languageCode="en"
